15c34a40c92ce52f9b4d9cc5cc088212679ca240
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-02-09 21:05:43 -0500

    fixed responding to sebbys "update live"

--- src/events/sebbyreply.js
+++ src/events/sebbyreply.js
@@ -3,6 +3,7 @@ module.exports = {
     async execute(message) {
 
         if (message.author.id !== "1338314970303037440") return;
+        if (message.channel.id === "1265926416168517693") return;
         const voiceLines = [
             "I'm sorry Sebastian",
             "Sure thing Sebastian!",
