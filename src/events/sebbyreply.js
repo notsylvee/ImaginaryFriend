ccd2d43f6526e22d363ccede9fdb37fd3617b350
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-02-09 20:43:19 -0500

    added interaction with sebby

---
+++ src/events/sebbyreply.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: "messageCreate",
+    async execute(message) {
+
+        if (message.author.id !== "1338314970303037440") return;
+        const voiceLines = [
+            "I'm sorry Sebastian",
+            "Sure thing Sebastian!",
+            "As you wish!",
+            "Sure, will do."
+        ];
+        const voiceLine = voiceLines[Math.floor(Math.random() * voiceLines.length)];
+        message.channel.send(`${voiceLine}`);
+    },
+};
