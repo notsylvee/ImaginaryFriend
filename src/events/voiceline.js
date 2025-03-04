265f96ca169fc82770c32329ce5fb0cdd68175e2
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-03-03 19:19:41 -0500

    fixed bot breaking itself trying to send messages where it cant

--- src/events/voiceline.js
+++ src/events/voiceline.js
@@ -1,8 +1,11 @@
+const { PermissionFlagsBits } = require("discord.js");
+
 module.exports = {
     name: "messageCreate",
     async execute(message) {
 
         if (message.author.bot || !message.guild) return;
+        if (!message.guild.members.me.permissionsIn(message.channel.id).has(PermissionFlagsBits.SendMessages)) return;
         const chance = Math.random() * 250;
         const voiceLines = [
             "H-hello.",
